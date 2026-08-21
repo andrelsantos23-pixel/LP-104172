1a00287714a80350cdbf76bfeaab5da533c0b1b5
andrelsantos23-pixel <andre.l.santos23@ba.estudante.senai.br>
2026-08-21 16:45:50 -0300

    Add files via upload

---
+++ 6_antecessor_sucessor.py
@@ -0,0 +1,11 @@
+import os 
+os.system('cls')
+
+numero = int(input("Digite algum numero: "))
+
+Sucessor = numero + 1
+antecessor = numero -1
+
+print('Número: ', numero)
+print('Antecessor', antecessor)
+print('Sucessor', Sucessor)
